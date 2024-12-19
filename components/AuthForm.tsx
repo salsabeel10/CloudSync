228cf169c978064a746f273f63a5ac87700b2e95
salsabeel10 <salsabeelibrahim327@gmail.com>
2024-12-19 20:19:46 +0400

    auth design completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,16 +16,23 @@ import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const formSchema = z.object({
-  fullName: z.string().min(2).max(50),
-  email: z.string().min(2).max(50),
-})
 type FormType = 'sign-in' | 'sign-up'
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === 'sign-up'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  })
+}
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false)
   const [errorMsg, setErrorMSg] = useState("")
-
+  
+  const formSchema = authFormSchema(type)
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {

aa0383c7b781d4b6091d3b5595a4e68b62ac28a0
salsabeel10 <salsabeelibrahim327@gmail.com>
2024-12-19 13:00:28 +0400

    auth layout completed

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const SiginIn = () => <AuthForm type="sign-in" />
+export default SiginIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const SiginUp = () => <AuthForm type="sign-in" />
+export default SiginUp
